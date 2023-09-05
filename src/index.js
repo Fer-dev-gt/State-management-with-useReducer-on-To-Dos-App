ebe5a4245365975e1000ac074e1cdac4be00933d
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-09-05 17:50:40 -0600

    Example on a High Order Component on App/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 // Este es el Entry Point de la aplicación, va a mostrar los componentes en el div 'root'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import App from './App';                                            // Importamos el Componente 'App' del archivo 'App.js'
 import './index.css';
-import App from './App';                                            // Importamos el Componente 'App' del archivo 'App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Creamos un Root para React
-root.render(<App />);                                               // Renderizamos el Componente 'App' dentro del 'root' que creamos
+function App(props){
+  return (
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  );
+}
+
+function withWhatever(WrappedComponent) {
+  return function ComponenteDeVerdad(props) {
+    return(
+      <>
+        <WrappedComponent {...props}/>                                {/* Destrucuramos las props del Componente que es enviado en como parámetro */}
+        <p>Estamos acompañando al WrappedComponent</p>
+      </>
+    )
+  }
+}
+  
+const AppWithWhatever = withWhatever(App);                             // Ejecutamos la HOC y nos devuelve un Componente
+
+const root = ReactDOM.createRoot(document.getElementById('root'));     // Creamos un Root para React
+// root.render(<App />);                                               // Renderizamos el Componente 'App' dentro del 'root' que creamos
+
+root.render(<AppWithWhatever saludo="Buenas Tarde" nombre="Fernando"
+
+/>)
